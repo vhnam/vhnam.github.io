117dc38bbbb19010a19ef1b2577a1413264197a6
namvh <vhnam2504@gmail.com>
2015-01-23 18:23:12 +0700

    edit Index Pagination.

--- public/js/post.js
+++ public/js/post.js
@@ -9,5 +9,18 @@ angular.module('index', [])
 	.controller('IndexController', function($scope, Post) {
 		Post.get().then(function(collection) {
 			$scope.postCollection = collection.data;
+			$scope.currentPage = 0;
+			$scope.pageSize = 20;
+
+			$scope.numberOfPages = function(){
+				return Math.ceil($scope.postCollection.length / $scope.pageSize);
+			}
 		});
-	});
+	})
+	.filter('startFrom', function() {
+		return function(input, start) {
+			start = +start; //parse to int
+			if (input != undefined)
+				return input.slice(start);
+		}
+	});;
