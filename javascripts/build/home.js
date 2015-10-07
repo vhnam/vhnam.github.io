8f6c577cd4368a60e99aff9bf5548b48bc05b522
vhnam <vhnam2504@gmail.com>
2015-10-08 01:01:37 +0700

    edit Homepage.

---
+++ javascripts/build/home.js
@@ -0,0 +1,58 @@
+"use strict";
+
+(function () {
+
+    var ListRow = React.createClass({
+        displayName: "ListRow",
+
+        render: function render() {
+            return React.createElement(
+                "li",
+                { className: "list-field" },
+                React.createElement(
+                    "a",
+                    { href: this.props.blog.link, className: "title" },
+                    this.props.blog.name
+                ),
+                React.createElement(
+                    "div",
+                    { className: "time" },
+                    React.createElement(
+                        "time",
+                        { dateTime: this.props.blog.datetime, itemProp: "datePublished" },
+                        this.props.blog.time
+                    )
+                ),
+                React.createElement(
+                    "p",
+                    { className: "description" },
+                    this.props.blog.description
+                )
+            );
+        }
+    });
+
+    var List = React.createClass({
+        displayName: "List",
+
+        render: function render() {
+            var list = [];
+
+            this.props.blogs.forEach(function (blog) {
+                list.push(React.createElement(ListRow, { blog: blog, key: blog.name }));
+            });
+
+            return React.createElement(
+                "ul",
+                { className: "list" },
+                list
+            );
+        }
+    });
+
+    $(document).ready(function () {
+        $.getJSON('data/blogs.json', function (data) {
+            React.render(React.createElement(List, { blogs: data.blogs }), document.getElementById('blog-list'));
+        });
+    });
+})();
---
+++ javascripts/home.js
@@ -0,0 +1,43 @@
+(function() {
+
+var ListRow = React.createClass({
+    render: function() {
+        return (
+            <li className="list-field">
+                <a href={this.props.blog.link} className="title">{this.props.blog.name}</a>
+                <div className="time">
+                    <time dateTime={this.props.blog.datetime} itemProp="datePublished">{this.props.blog.time}</time>
+                </div>
+                <p className="description">{this.props.blog.description}</p>
+            </li>
+        );
+    }
+});
+
+var List = React.createClass({
+    render: function() {
+        var list = [];
+
+        this.props.blogs.forEach(function(blog) {
+            list.push(
+                <ListRow blog={blog} key={blog.name} />
+            );
+        });
+
+        return (
+            <ul className="list">
+                {list}
+            </ul>
+        );
+    }
+});
+
+$(document).ready(function() {
+    $.getJSON('data/blogs.json', function(data) {
+        React.render(
+            <List blogs={data.blogs} />
+        , document.getElementById('blog-list'));
+    })
+});
+
+})();
