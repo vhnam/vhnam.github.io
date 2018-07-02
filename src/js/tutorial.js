cb6ee402a3fcb8a8dbfa286e2fcbb7d874858c47
Nam Vo <vhnam2504@gmail.com>
2018-07-02 10:24:39 +0700

    upgrade to 3.0

---
+++ src/js/tutorial.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function() {
+    getTutorials();
+}, false);
+
+function getTutorials() {
+    axios.get('https://vhnam.github.io/src/indexes/tutorials.json').then(function(res) {
+        if (200 === res.status) {
+            let diaries = res.data.blogs;
+            let diaryNode = document.getElementById('blog-list');
+
+            render(diaryNode, diaries);
+        }
+    }).catch((err) => { console.error(err) });
+}
+
+function render(container, list) {
+    list.forEach(element => {
+        title = document.createElement('h4');
+        title.className = 'item__title';
+        title.innerText = element.name;
+
+        time = document.createElement('span');
+        time.className = 'item__time';
+        time.innerText = element.time;
+
+        description = document.createElement('p');
+        description.className = 'item__description';
+        description.innerText = element.description;
+
+        link = document.createElement('a');
+        link.className = 'list__item';
+        link.href = element.link;
+        link.setAttribute('itemprop', 'url');
+        link.appendChild(title);
+        link.appendChild(time);
+        link.appendChild(description);
+
+        li = document.createElement('li');
+        li.appendChild(link);
+
+        container.appendChild(li);
+    });
+}
---
+++ webpack.mix.js
@@ -0,0 +1,26 @@
+let mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for your application, as well as bundling up your JS files.
+ |
+ */
+
+mix
+    .js('src/js/bootstrap.js', 'public/js')
+    .js('src/js/index.js', 'public/js')
+    .js('src/js/blog.js', 'public/js')
+    .js('src/js/tutorial.js', 'public/js')
+
+    .sass('src/scss/bootstrap.scss', 'public/css')
+    .sass('src/scss/index.scss', 'public/css')
+    .sass('src/scss/contact.scss', 'public/css')
+    .sass('src/scss/about.scss', 'public/css')
+    .sass('src/scss/post.scss', 'public/css')
+    .sass('src/scss/list.scss', 'public/css')
+;
