79e6a4ab9c5d964f57dabc4f12a804e0ad0336be
Nam Vo <vhnam2504@gmail.com>
2018-08-06 05:33:10 +0700

    edit About page

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,145 @@
-!function(e){var t={};function n(i){if(t[i])return t[i].exports;var o=t[i]={i:i,l:!1,exports:{}};return e[i].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=e,n.c=t,n.d=function(e,t,i){n.o(e,t)||Object.defineProperty(e,t,{configurable:!1,enumerable:!0,get:i})},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="/",n(n.s=39)}({39:function(e,t,n){e.exports=n(40)},40:function(e,t){function n(e,t){for(var n=0;n<5;n++)element=t[n],title=document.createElement("h4"),title.className="item__title",title.innerText=element.name,time=document.createElement("span"),time.className="item__time",time.innerText=element.time,description=document.createElement("p"),description.className="item__description",description.innerText=element.description,link=document.createElement("a"),link.className="list__item",link.setAttribute("itemprop","url"),link.href=element.link,link.appendChild(title),link.appendChild(time),link.appendChild(description),li=document.createElement("li"),li.appendChild(link),e.appendChild(li)}document.addEventListener("DOMContentLoaded",function(){axios.get("https://vhnam.github.io/src/indexes/blog.json").then(function(e){if(200===e.status){var t=e.data.blog,i=document.getElementById("blog-list");console.log(e.data),n(i,t)}}).catch(function(e){console.error(e)}),axios.get("https://vhnam.github.io/src/indexes/tutorials.json").then(function(e){if(200===e.status){var t=e.data.tutorials,i=document.getElementById("tutorial-list");n(i,t)}}).catch(function(e){console.error(e)})},!1)}});
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 39);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 39:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(40);
+
+
+/***/ }),
+
+/***/ 40:
+/***/ (function(module, exports) {
+
+document.addEventListener('DOMContentLoaded', function () {
+    getDiaries();
+    getTutorials();
+}, false);
+
+function getDiaries() {
+    axios.get('https://vhnam.github.io/src/indexes/blog.json').then(function (res) {
+        if (200 === res.status) {
+            var diaries = res.data.blog;
+            var diaryNode = document.getElementById('blog-list');
+
+            console.log(res.data);
+
+            render(diaryNode, diaries);
+        }
+    }).catch(function (err) {
+        console.error(err);
+    });
+}
+
+function getTutorials() {
+    axios.get('https://vhnam.github.io/src/indexes/tutorials.json').then(function (res) {
+        if (200 === res.status) {
+            var tutorials = res.data.tutorials;
+            var tutorialNode = document.getElementById('tutorial-list');
+
+            render(tutorialNode, tutorials);
+        }
+    }).catch(function (err) {
+        console.error(err);
+    });
+}
+
+function render(container, list) {
+    for (var index = 0; index < 5; index++) {
+        element = list[index];
+
+        title = document.createElement('h4');
+        title.className = 'item__title';
+        title.innerText = element.name;
+
+        time = document.createElement('span');
+        time.className = 'item__time';
+        time.innerText = element.time;
+
+        description = document.createElement('p');
+        description.className = 'item__description';
+        description.innerText = element.description;
+
+        link = document.createElement('a');
+        link.className = 'list__item';
+        link.setAttribute('itemprop', 'url');
+        link.href = element.link;
+        link.appendChild(title);
+        link.appendChild(time);
+        link.appendChild(description);
+
+        li = document.createElement('li');
+        li.appendChild(link);
+
+        container.appendChild(li);
+    }
+}
+
+/***/ })
+
+/******/ });
--- public/js/tutorial.js
+++ public/js/tutorial.js
@@ -1,1 +1,127 @@
-!function(e){var t={};function n(i){if(t[i])return t[i].exports;var r=t[i]={i:i,l:!1,exports:{}};return e[i].call(r.exports,r,r.exports,n),r.l=!0,r.exports}n.m=e,n.c=t,n.d=function(e,t,i){n.o(e,t)||Object.defineProperty(e,t,{configurable:!1,enumerable:!0,get:i})},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="/",n(n.s=43)}({43:function(e,t,n){e.exports=n(44)},44:function(e,t){document.addEventListener("DOMContentLoaded",function(){axios.get("https://vhnam.github.io/src/indexes/tutorials.json").then(function(e){if(200===e.status){var t=e.data.tutorials,n=document.getElementById("blog-list");i=n,t.forEach(function(e){title=document.createElement("h4"),title.className="item__title",title.innerText=e.name,time=document.createElement("span"),time.className="item__time",time.innerText=e.time,description=document.createElement("p"),description.className="item__description",description.innerText=e.description,link=document.createElement("a"),link.className="list__item",link.href=e.link,link.setAttribute("itemprop","url"),link.appendChild(title),link.appendChild(time),link.appendChild(description),li=document.createElement("li"),li.appendChild(link),i.appendChild(li)})}var i}).catch(function(e){console.error(e)})},!1)}});
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 43);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 43:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(44);
+
+
+/***/ }),
+
+/***/ 44:
+/***/ (function(module, exports) {
+
+document.addEventListener('DOMContentLoaded', function () {
+    getTutorials();
+}, false);
+
+function getTutorials() {
+    axios.get('https://vhnam.github.io/src/indexes/tutorials.json').then(function (res) {
+        if (200 === res.status) {
+            var diaries = res.data.tutorials;
+            var diaryNode = document.getElementById('blog-list');
+
+            render(diaryNode, diaries);
+        }
+    }).catch(function (err) {
+        console.error(err);
+    });
+}
+
+function render(container, list) {
+    list.forEach(function (element) {
+        title = document.createElement('h4');
+        title.className = 'item__title';
+        title.innerText = element.name;
+
+        time = document.createElement('span');
+        time.className = 'item__time';
+        time.innerText = element.time;
+
+        description = document.createElement('p');
+        description.className = 'item__description';
+        description.innerText = element.description;
+
+        link = document.createElement('a');
+        link.className = 'list__item';
+        link.href = element.link;
+        link.setAttribute('itemprop', 'url');
+        link.appendChild(title);
+        link.appendChild(time);
+        link.appendChild(description);
+
+        li = document.createElement('li');
+        li.appendChild(link);
+
+        container.appendChild(li);
+    });
+}
+
+/***/ })
+
+/******/ });
