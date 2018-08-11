8df868e184306bc13205cb80f98ee35c52d88480
Nam Vo <vhnam2504@gmail.com>
2018-08-11 18:09:53 +0700

    add tutorial: distributed-system-cac-kieu-con-da-dieu

--- src/scripts/tutorials.py
+++ src/scripts/tutorials.py
@@ -77,7 +77,7 @@ def main():
     # for directory in directories:
     #     build(directory, 'tutorials')
 
-    build("distributed-system", 'tutorials')
+    build("distributed-system-cac-kieu-con-da-dieu", 'tutorials')
 
 
 
