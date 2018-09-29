5f89c959ea024ae79c923c323a4e77850a9c04fb
Nam Vo <vhnam2504@gmail.com>
2018-09-30 02:37:55 +0700

    fix navigation, restore homepage

--- src/assets/js/bootstrap.js
+++ src/assets/js/bootstrap.js
@@ -20,26 +20,3 @@ $.fn.isInViewport = function() {
   
     return elementBottom > viewportTop && elementTop < viewportBottom;
 };
-
-
-
-var index = $("#index");
-var position = index.position();
-
-$(window).on('resize scroll', function() {
-    $('.heading').each(function() {
-        const id = $(this).attr('id');
-        if ($(this).isInViewport()) {
-            $('.index__item').removeClass('index--highlight');
-            $(`#prefix-${id}`).addClass('index--highlight');
-        }
-    });
-    
-    const windowPosition = $(window).scrollTop();
-
-    if (windowPosition >= position.top) {
-        index.addClass("index--sticked");
-    } else {
-        index.removeClass("index--sticked"); 
-    }
-});
---
+++ src/assets/js/index.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', function() {
+    getDiaries();
+    getTutorials();
+}, false);
+
+function getDiaries() {
+    axios.get('https://vhnam.github.io/src/index/blog.json').then(function(res) {
+        if (200 === res.status) {
+            let diaries = res.data.blog;
+            let diaryNode = document.getElementById('blog-list');
+
+            render(diaryNode, diaries);
+        }
+    }).catch((err) => { console.error(err) });
+}
+
+function getTutorials() {
+    axios.get('https://vhnam.github.io/src/index/tutorials.json').then(function(res) {
+        if (200 === res.status) {
+            let tutorials = res.data.tutorials;
+            let tutorialNode = document.getElementById('tutorial-list');
+
+            render(tutorialNode, tutorials);
+        }
+    }).catch((err) => { console.error(err) });
+}
+
+function render(container, list) {   
+    for (let index = 0; index < 5; index++) {
+        element = list[index];
+
+        title = document.createElement('a');
+        title.className = 'item__title';
+        title.href = element.link;
+        title.innerText = element.name;
+
+        time = document.createElement('span');
+        time.className = 'item__time';
+        time.innerText = element.time;
+
+        description = document.createElement('p');
+        description.className = 'item__description';
+        description.innerText = element.description;
+
+        li = document.createElement('li');
+        li.appendChild(title);
+        li.appendChild(time);
+        li.appendChild(description);
+
+        container.appendChild(li);
+    }
+}
