1d85b4d822d9b323fc8058134f8429413407ec4d
Nam Vo <vhnam2504@gmail.com>
2019-09-07 19:36:51 +0700

    refactor(stories): update But You Didn't

--- stories/but-you-didnt/service-worker.js
+++ stories/but-you-didnt/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1395dd5d2a5b9af335c2d8e6d8faa0dc.js"
+  "/precache-manifest.ae56031e0c77834bb2a6e682fc37c771.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
