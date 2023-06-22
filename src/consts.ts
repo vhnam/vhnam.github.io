72c72e649bf493660ea2ae7591e81627d7eeb9cc
Nam Vo <vhnam2504@gmail.com>
2023-06-23 02:40:54 +0700

    feat(tutorial): add lam-lai-blog-voi-astro

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
-export const SITE_TITLE = "Nam Vo's Blog";
+export const SITE_TITLE = "Nam Vo";
 export const SITE_DESCRIPTION = "Nam Vo | Software Engineer";
 
 export const GA_MEASUREMENT_ID = "G-NEXD10J18C";
