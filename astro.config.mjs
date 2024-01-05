9018591fa9a5ec2b45dca4c2e1dcd553c5eb8690
Nam Vo <vhnam2504@gmail.com>
2024-01-05 18:19:04 +0700

    chore: update Astro v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,33 @@
+import { defineConfig, squooshImageService } from "astro/config";
 import mdx from "@astrojs/mdx";
 import partytown from "@astrojs/partytown";
+import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), partytown({
-    config: {
-      forward: ["dataLayer.push", "FB.init"]
-    }
-  }), sitemap(), tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), react()],
+  image: {
+    service: squooshImageService(),
+  },
+  integrations: [
+    mdx(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push", "FB.init"],
+      },
+    }),
+    sitemap(),
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    react(),
+  ],
   markdown: {
     theme: "github-dark",
-    wrap: true
+    wrap: true,
   },
-  site: "https://vhnam.github.io"
+  site: "https://vhnam.github.io",
 });
