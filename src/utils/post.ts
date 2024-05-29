ae7d17e71108114b70138ebda38aa042f264b6bd
Nam Vo <vhnam2504@gmail.com>
2024-05-29 23:35:05 +0700

    feat: add Tags

--- src/utils/post.ts
+++ src/utils/post.ts
@@ -1,14 +1,40 @@
+import slugify from "slugify";
+
 export const getTags = (posts: any) => {
-  const tags = {} as Record<string, number>;
+  const tags = {} as Record<
+    string,
+    {
+      posts: any[];
+      total: number;
+    }
+  >;
 
   posts.forEach((post: any) => {
     post.data.tags?.forEach((tag: string) => {
-      const value = tags[tag];
-      tags[tag] = value ? value + 1 : 1;
+      if (undefined === tags[tag]) {
+        tags[tag] = {
+          total: 0,
+          posts: [],
+        };
+      }
+
+      tags[tag].total = tags[tag].total + 1;
+      tags[tag].posts.push(post);
     });
   });
 
   return Object.entries(tags)
-    .sort(([, a], [, b]) => b - a)
-    .reduce((obj, [key, value]) => ({ ...obj, [key]: value }), {});
+    .sort(([, a], [, b]) => b.total - a.total)
+    .reduce((obj, [key, value]) => {
+      const slug = slugify(key, {
+        locale: "vi",
+      }).toLowerCase();
+
+      return Object.assign(obj, {
+        [slug]: {
+          name: key,
+          ...value,
+        },
+      });
+    }, {});
 };
