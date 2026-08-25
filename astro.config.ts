3f3cd29d5b2f32be354f14991a118f47d38234e9
Nam Vo <vhnam2504@gmail.com>
2026-08-25 18:37:45 +0700

    chore: remove Svelte, add slugify

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,10 @@
 import { unified } from "@astrojs/markdown-remark";
 import mdx from "@astrojs/mdx";
-import svelte from "@astrojs/svelte";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  integrations: [mdx(), svelte()],
+  integrations: [mdx()],
   markdown: {
     processor: unified(),
   },
